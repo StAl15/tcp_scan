2fb78e688a44b5cab820211e2575dd1ae011203b
StAl15 <stihinaleksander@yandex.ru>
2022-06-09 19:01:37 +0500

    initial

---
+++ main.py
@@ -0,0 +1,29 @@
+import aiohttp
+from aiohttp import web
+import asyncio
+import json
+from scanner import *
+
+
+async def handle(request):
+    response_obj = {' status ': 'success'}
+    return web.Response(text=json.dumps(response_obj), status=200)
+
+
+async def scan(request):
+    try:
+        ip = request.query['ip']
+        begin_port = request.query['begin_port']
+        end_port = request.query['end_port']
+        result = receive_open_ports(ip, int(begin_port), int(end_port))
+        result = {'ip': ip, 'result': [result]}
+        return web.Response(text=json.dumps(result), status=200)
+    except:
+        print(Exception, ' something went wrong')
+
+
+app = web.Application()
+app.router.add_get('/', handle)
+app.router.add_get('/scan', scan)
+
+web.run_app(app)
---
+++ scanner.py
@@ -0,0 +1,25 @@
+import socket
+
+
+def scan_port(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(0.5)
+    result = False
+    try:
+        connect = sock.connect((ip, port))
+        result = True
+        sock.close()
+    except:
+        pass
+    return result
+
+
+def receive_open_ports(ip, begin_port, end_port):
+    open_ports = {}
+    for i in range(begin_port, end_port + 1):
+        # print(open_ports)
+        if scan_port(ip, i):
+            open_ports[i] = "open"
+            continue
+        open_ports[i] = "close"
+    return open_ports
